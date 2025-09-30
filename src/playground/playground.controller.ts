ef4bee491a193b269b57e201b5242643220cb6a4
syukranDev <m.syukransoleh@amast.com.my>
2025-09-30 15:21:10 +0800

    save learning progress

--- src/playground/playground.controller.ts
+++ src/playground/playground.controller.ts
@@ -1,21 +1,23 @@
 import { Controller, Get, Post, Param, Query, Body } from '@nestjs/common';
-
+import { PlaygroundService } from './playground.service';
 @Controller('playground')
 export class PlaygroundController {
+    //GET http://localhost:3000/playground
     @Get()
     findAll(@Query('sort') sort: 'asc' | 'desc' = 'desc') {
         console.log(sort)
         return 'this is all data';
     }
 
-    @Get('featured')
-    findFeatured() {
-        return 'this is featured data'
+    //http://localhost:3000/playground/featured
+    @Get('featured') 
+    findFeatured() { //boleh letak any method name kita nak
+        return this.playgroundService.findFeatured()
     }
 
 
-
-    @Post()
+    //POST http://localhost:3000/playground
+    @Post() //
     create(@Body() body: any) {
         console.log(body) 
         return 'submit payload data'
--- src/playground/playground.module.ts
+++ src/playground/playground.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PlaygroundController } from './playground.controller';
+import { PlaygroundService } from './playground.service';
 
 @Module({
-  controllers: [PlaygroundController]
+  controllers: [PlaygroundController], // notedev: manage route API
+  providers: [PlaygroundService] // notedev: manage api logic
 })
 export class PlaygroundModule {}
