b6d760d1550611a8d5bfca42e92829744eb86f58
syukranDev <m.syukransoleh@amast.com.my>
2025-09-30 14:51:04 +0800

    create users, posts, tags modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostsModule } from './posts/posts.module';
+import { UsersModule } from './users/users.module';
+import { TagsModule } from './tags/tags.module';
 
 @Module({
-  imports: [],
+  imports: [PostsModule, UsersModule, TagsModule],
   controllers: [AppController],
   providers: [AppService],
 })
