8db6f389b185b526879b73c3b8f4b2c9650a51bf
syukranDev <syukran.dev@gmail.com>
2025-10-02 16:28:13 +0800

    Update posts controller to all using CQRS instead of old service

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -4,14 +4,18 @@ import { ValidationPipe } from '@nestjs/common';
 import { CreatePostDto } from './dto/createPost.dto';
 import { UpdatePostDto } from './dto/updatePost.dto';
 import { JwtAuthGuard } from 'src/guards/jwt-auth.guard';
-import { CommandBus } from '@nestjs/cqrs';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { CreatePostCommand } from './commands/create-post.command';
+import { GetUserPostsQuery } from './queries/get-user-posts.query';
+import { UpdatePostCommand } from './commands/update-post.command';
+import { DeletePostCommand } from './commands/delete-post.command';
 
 @Controller('posts')
 export class PostsController {
     constructor(
         private readonly postsService: PostsService,
         private readonly commandBus: CommandBus,
+        private readonly queryBus: QueryBus,
     ) {}
 
     @UseGuards(JwtAuthGuard)
@@ -20,13 +24,15 @@ export class PostsController {
         // let username = 'superadmin'
         let username = req.user.username;
 
-        // return this.postsService.createNewPost(username, createPostDto) --- notedev: old way using service 
-        return this.commandBus.execute(new CreatePostCommand( // notedev: new using CQRS pattern
+        return this.commandBus.execute(new CreatePostCommand( 
             username, 
             createPostDto.title,
             createPostDto.description,
             createPostDto.tags
         ))
+
+        // #notedev: old way using service
+        // return this.postsService.createNewPost(username, createPostDto)  
     }
 
     @UseGuards(JwtAuthGuard)
@@ -34,7 +40,10 @@ export class PostsController {
     getAllPosts(@Request() req: any) {
         // let username = 'superadmin'
         let username = req.user.username;
-        return this.postsService.getAllPostsByUser(username)
+        return this.queryBus.execute(new GetUserPostsQuery(username))
+
+        // #notedev: new way using CQRS pattern
+        // return this.postsService.getAllPostsByUser(username)
     }
 
     @UseGuards(JwtAuthGuard)
@@ -42,7 +51,17 @@ export class PostsController {
     updatePost(@Request() req: any, @Body(ValidationPipe) updatePostDto: UpdatePostDto, @Param('id') id: number) {
         // let username = 'superadmin'
         let username = req.user.username;
-        return this.postsService.updatePostByUser(username, id, updatePostDto)
+        
+        return this.commandBus.execute(new UpdatePostCommand(
+            username, 
+            id, 
+            updatePostDto.title,
+            updatePostDto.description,
+            updatePostDto.tags
+        ))
+
+        // #notedev: old way using service
+        // return this.postsService.updatePostByUser(username, id, updatePostDto)
     }
 
     @UseGuards(JwtAuthGuard)
@@ -50,6 +69,13 @@ export class PostsController {
     deletePost(@Request() req: any, @Param('id') id: number) {
         // let username = 'superadmin'
         let username = req.user.username;
-        return this.postsService.deletePostByUser(username, id)
+
+        return this.commandBus.execute(new DeletePostCommand(
+            username, 
+            id
+        ))
+
+        // #notedev: old way using service
+        // return this.postsService.deletePostByUser(username, id)
     }
 }
